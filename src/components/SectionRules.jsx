bb57987007efc21f88ab3acd93d7a1b0f4cf7baf
lettiagc <letti.agc@gmail.com>
2024-01-03 02:28:40 +0100

    Added Rules Section

--- src/components/SectionRules.jsx
+++ src/components/SectionRules.jsx
@@ -1,10 +1,31 @@
 import SectionHeader from "./SectionHeader";
+import TextCard from "./TextCard";
+import Hat from "../assets/mdi_wizard-hat.svg";
+import Tournament from "../assets/tabler_tournament.svg";
+import Reward from "../assets/material-symbols_rewarded-ads.svg";
 
 function SectionRules() {
   return (
     <>
-      <section className="min-h-svh w-full px-4 md:px-8 lg:px-6">
+      <section className="w-full px-4 md:px-8 lg:px-6">
         <SectionHeader heading="How It Works" />
+        <div className="flex flex-col gap-4 py-4 md:flex-row md:py-8 lg:gap-6 lg:py-16">
+          <TextCard
+            icon={Hat}
+            title="Step 1"
+            text="Sign Up – Create your magical profile."
+          />
+          <TextCard
+            icon={Tournament}
+            title="Step 2"
+            text="Choose a Tournament – Pick from a variety of dueling events."
+          />
+          <TextCard
+            icon={Reward}
+            title="Step 3"
+            text="Compete & Win – Duel with others and climb the leaderboard."
+          />
+        </div>
       </section>
     </>
   );
