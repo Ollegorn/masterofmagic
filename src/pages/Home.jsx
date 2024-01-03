3f8efa861d119e137c9d290b40a45010e5707bea
lettiagc <letti.agc@gmail.com>
2024-01-03 03:07:30 +0100

    Added CTA

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,59 @@ function Home() {
         <SectionEvents />
         <SectionHighlights />
         <SectionRules />
+        <section className="flex w-full flex-col gap-4 bg-hero-pattern bg-cover bg-fixed bg-center bg-no-repeat px-4 py-24 md:px-8 lg:gap-6 lg:px-6 lg:py-40">
+          <div className=" flex flex-1 flex-col items-center justify-center gap-4 self-stretch lg:gap-6">
+            <svg viewBox="0 0 50 16" className="w-full">
+              <text
+                className="font-body"
+                x="0"
+                y="15"
+                fill="url(#paint0_linear_12_795)"
+              >
+                Master
+              </text>
+              <defs>
+                <linearGradient
+                  id="paint0_linear_12_795"
+                  x1="30.876"
+                  y1="0.765625"
+                  x2="2.29087"
+                  y2="8.2084"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop offset="0.421875" stop-color="#C2C2AD" />
+                  <stop offset="0.588542" stop-color="#81805F" />
+                  <stop offset="0.917708" stop-color="#81805F" />
+                </linearGradient>
+              </defs>
+            </svg>
+
+            <svg viewBox="0 0 73 19" className="w-full">
+              <text
+                className="font-body"
+                x="0"
+                y="15"
+                fill="url(#paint0_linear_12_795)"
+              >
+                The Magic
+              </text>
+              <defs>
+                <linearGradient
+                  id="paint0_linear_12_795"
+                  x1="30.876"
+                  y1="0.765625"
+                  x2="2.29087"
+                  y2="8.2084"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop offset="0.421875" stop-color="#C2C2AD" />
+                  <stop offset="0.588542" stop-color="#81805F" />
+                  <stop offset="0.917708" stop-color="#81805F" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
+        </section>
       </div>
     </>
   );
