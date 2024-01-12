f00d76757d68a681a12f968efe046b6c46da44a1
John Kakouris <jdkakouris@gmail.com>
2024-01-12 02:03:00 +0100

    working on login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useUser } from '../hooks/useUser';
 import Header from './Header';
+import InputField from './InputField';
+import Button from './Button';
 
 function Login({ onSignupClick, onButtonClick }) {
   const [username, setUsername] = useState('');
@@ -26,11 +28,10 @@ function Login({ onSignupClick, onButtonClick }) {
     e.preventDefault();
 
     try {
-      // Your fetch logic remains the same
+      // fetch logic
 
       if (response.ok) {
-        // Your success logic remains the same
-
+        // success
         setLoginMessage('Login successful! Redirecting...');
         setTimeout(() => {
           setLoginMessage('');
@@ -47,9 +48,16 @@ function Login({ onSignupClick, onButtonClick }) {
   };
 
   return (
-    <div className="flex min-w-72 max-w-180 p-6 gap-6 flex-col items-center rounded-3xl bg-opacity-0">
-      <div className='flex p-0 flex-col items-start gap-6 self-stretch rounded-none'>
-
+    <div className="flex min-w-72 w-96 max-w-180 p-6 gap-6 flex-col items-center rounded-3xl bg-opacity-0">
+      <InputField icon="email" label="Email" placeholderText="user@email.com" />
+      <InputField icon="lock" label="Password" placeholderText="●●●●●●●●" isPassword/>
+      <div className="flex p-6 flex-col items-start self-stretch">
+        <Button className="flex p-6 justify-center items-center self-stretch rounded">Log In</Button>
+      </div>
+      <div className='flex justify-center items-center self-center'>
+        <button className="transition-transform duration-300 transform hover:translate-y-[-3px] hover:shadow-glow">
+          Don't have an account? <span className="inline-block bg-gradient-to-r from-primary04-500 to-primary04-50 bg-clip-text font-body text-transparent">Sign Up</span>
+        </button>
       </div>
     </div>
   );
