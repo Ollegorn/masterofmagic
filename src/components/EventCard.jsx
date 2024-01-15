b5a4105702fd370d005be5f01ec0308aeb1c4ee7
lettiagc <letti.agc@gmail.com>
2024-01-15 08:46:15 +0100

    adjusted hover scale on EventCards

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -14,7 +14,7 @@ function EventCard({
 }) {
   return (
     <>
-      <div className="flex max-w-sm cursor-pointer flex-col-reverse justify-between rounded-lg bg-gradient-to-b from-primary04-800/15 to-primary04-800/50 backdrop-blur-xl transition-all duration-300 hover:scale-95 hover:shadow-glow focus:scale-95 focus:shadow-glow md:max-w-2xl md:flex-row lg:max-w-4xl">
+      <div className="flex max-w-sm cursor-pointer flex-col-reverse justify-between rounded-lg bg-gradient-to-b from-primary04-800/15 to-primary04-800/50 backdrop-blur-xl transition-all duration-300 ease-in-out hover:shadow-glow focus:shadow-glow md:max-w-2xl md:flex-row lg:max-w-4xl">
         <div className="flex min-w-60 flex-col items-center justify-center gap-4 self-stretch p-4 lg:min-w-96 lg:p-6">
           <div className="flex flex-wrap gap-1 self-stretch lg:gap-2">
             <Badge label={type} />
